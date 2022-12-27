ae0cf2e165ff063b1ff37aaabe1b7a6467e47dcf
Souvik Dey <deysouvik700@gmail.com>
2022-12-27 19:58:02 +0530

    fixed fallback issue

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths(){
    const result=await db.collection('meetups').find({},{_id:1}).toArray()           
    clint.close()
    return {
-      fallback: true,
+      fallback: 'blocking',
       paths: result.map(meetupId=>({
          params:{
             meetupID:meetupId._id.toString()
