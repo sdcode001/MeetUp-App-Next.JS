1503984f08704c5a7b380773ba49d6fbe3b651a6
Souvik Dey <92887905+sdcode001@users.noreply.github.com>
2023-01-11 15:09:56 +0530

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -7,7 +7,7 @@ async function newMeeting(req,res){
         //saving data to MongoDB database   
         //here we are ignoring error handling using try-catch just to keep it simple 
         
-                const client= await MongoClient.connect('mongodb+srv://sdcode001:ZlDtftXH13xexart@cluster0.3yr72o9.mongodb.net/database1?retryWrites=true&w=majority')
+                const client= await MongoClient.connect('mongodb+srv://sdcode001:j92txQCN7mUJWGmx@cluster0.3yr72o9.mongodb.net/database1?retryWrites=true&w=majority')
                 const db=client.db()
                 const result=await db.collection('meetups').insertOne(newMeetData)
                 console.log(result)
